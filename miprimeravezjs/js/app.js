c1a7dcf79b043b371291045b2d66d43a8a77032c
Vicky <jvirginiamonserrat@gmail.com>
2025-03-24 20:03:51 -0600

    jotos

---
+++ miprimeravezjs/js/app.js
@@ -0,0 +1,40 @@
+const suma=()=>{
+try{
+    let num1=parseInt(prompt("numero 1:"));
+    let num2=parseInt(prompt("numero 2:"));
+let sum=num1+num2;
+alert("la suma es:  "+sum);
+}catch(e){
+
+}
+}
+const resta=()=>{
+    try{
+        let num1=parseInt(prompt("numero 1:"));
+        let num2=parseInt(prompt("numero 2:"));
+    let res=num1-num2;
+    alert("la resta es:  "+res);
+    }catch(e){
+    
+    }
+    }
+        const divi=()=>{
+            try{
+                let num1=parseInt(prompt("numero 1:"));
+                let num2=parseInt(prompt("numero 2:"));
+            let div=num1/num2;
+            alert("la division es:  "+div);
+            }catch(e){
+            
+            }
+            }
+            const mult=()=>{
+                try{
+                    let num1=parseInt(prompt("numero 1:"));
+                    let num2=parseInt(prompt("numero 2:"));
+                let mul=num1*num2;
+                alert("la multiplicacion es:  "+mul);
+                }catch(e){
+                
+                }
+                }
